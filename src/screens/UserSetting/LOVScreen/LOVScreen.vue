f62a8124ce9ae8a0c62641b0fcfedc1da67fde44
sarawutduangdee <sarawutduangdee@gmail.com>
2020-03-09 16:30:36 +0700

    updateM

--- src/screens/UserSetting/LOVScreen/LOVScreen.vue
+++ src/screens/UserSetting/LOVScreen/LOVScreen.vue
@@ -1,12 +1,84 @@
 <template>
-    <div>
-        FFFF-3
+    <div id='lovHeaderId'>
+    <bw-card title='Lov'>
+      <bw-time-picker title='Date : '></bw-time-picker>
+      <BwTable 
+      :setClass="'row'"
+      @onEdit='onEditConstant'
+      :onClickTopBtn='onClickAdd'
+      :tableData='tableData'
+      :title='title' 
+      :lableEdit='lableButton'
+      :tableColumns='tableColumns'
+      :setStyle="{marginTop:'2%'}"
+      :propsToSearch='propsToSearch'
+      ></BwTable>
+    </bw-card>
     </div>
 </template>
 
 <script>
+import BwTable from '../../../components/BwTable/BwTable'
+import BwTimePicker from '../../../components/BwTimePicker/BwTimePicker'
+import BwCard from '../../../components/BwCard/BwCard'
+import Api from '../../../service/CallHttp'
     export default {
-        
+        name:'LOVScreen',
+        components:{
+        BwTable,
+        BwTimePicker,
+        BwCard
+        },
+        async created() {
+            this.tableData = await this.$store.getters.callApiGetLov;
+        },
+        computed: {
+            
+        },
+        methods: {
+            nextPage() {
+                this.$router.push("LovAdd");
+            },
+            onEditConstant(data) {
+                console.log(data);
+                this.$router.push({ name: "LovManage", params: { lovHeaderId: data } });
+            }
+        },
+       async created() {
+          let res = await (await Api()).getLovHeader() 
+        },
+        data() {
+            return {
+                tableData: [],
+                title: "test",
+                row:'row',
+                col:'col-md-12',
+                lableButton: "จัดการข้อมูล",
+                propsToSearch:["LovKey", "descripton"],
+                tableColumns: [
+                    {
+                         prop: 'lovHeaderId',
+                         label: 'ลำดับ', 
+                         minWidth: 50
+                    },
+                    {
+                         prop: 'lovKey',
+                         label: 'LovKey', 
+                         minWidth: 200
+                    },
+                    {
+                         prop: 'descripton',
+                         label: 'descripton', 
+                         minWidth: 200
+                    },
+                    
+                ],
+                onClickAdd: {
+                onClick: this.nextPage,
+                text: "Add"
+            }
+            }
+        },
     }
 </script>
 
---
+++ src/screens/UserSetting/LOVScreen/LovAdd.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="col-md-12 card">
+          <div class="row mt-3">
+      <div class="col-6">
+             <button
+         @click='goBack'
+          type="button"
+          class="btn btn-danger"
+        ><span class="btn-label"><i class=""></i></span>
+          ย้อนกลับ</button>
+      </div>
+           <div class="col-6">
+        <button
+         @click='goBack'
+          type="button"
+          class="btn btn-danger pull-right"
+        ><span class="btn-label"><i class="nc-icon nc-simple-remove"></i></span>
+          ยกเลิก</button>
+        <button
+          @click='validate'
+          type="button"
+          class="btn btn-success pull-right"
+        ><span class="btn-label"><i class="nc-icon nc-check-2"></i></span>
+          บันทึก </button>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-3">
+        <div class="form-group">
+          <label>LovKey    </label>
+          <fg-input
+           ref="name"
+            v-model="form.lovKey"
+            v-validate="formValidations.descripton"
+          >
+          </fg-input>
+        </div>
+        <div class="form-group">
+          <label>Descripton  </label>
+          <fg-input
+            v-model="form.lovKey"
+            v-validate="formValidations.descripton"
+          >
+          </fg-input>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CallHttp from '../../../service/CallHttp'
+export default {
+  
+  components: {
+  },
+    async created() {
+ 
+
+    
+        
+  },
+   data() {
+    return {
+      props: {
+        visible: {
+          type: Boolean,
+          default: false
+        }
+      },
+      isVisible: this.visible,
+      form: {
+        constantId:"",
+        lovKey: "",
+        descripton: "",  
+      },
+
+      formValidations: {
+        lovKey: {
+          required: true,
+        },
+        descripton: {
+          required: true,        
+        },
+      },
+    };
+  },
+    methods: {
+        validate() {
+          this.onSaveData()
+        },
+   
+       async onSaveData(){
+           let dataSave = await (await CallHttp()).getSaveLov(this.form)
+              this.$router.go(-1)
+        },
+        goBack(){
+          this.$router.go(-1)
+        }
+      }
+
+
+}
+</script>
+
