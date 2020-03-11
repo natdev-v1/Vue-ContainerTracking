5faf585deba6ebbf58b3e93fdc12f43ca08c4d7c
flame123flame <flame.tana@gmail.com>
2020-03-11 13:43:54 +0700

    add

---
+++ src/screens/ManageUser/user/user.vue
@@ -0,0 +1,101 @@
+<template>
+    <bw-card title='Lov Table'>
+    <!-- <button v-on:click="addForm()">5555</button> -->
+      <BwTable 
+      @onEdit='onEditLov'
+      :hiddenButtonCustom='hiddenButtonCustom'
+      :onClickTopCuttom='onClickTopCuttom'
+      :textCustom='textCustom'
+      :onClickTopBtn='onClickAdd'
+      :tableData='tableData'
+      :lableEdit='lableButton'
+      :tableColumns='tableColumns'
+      :propsToSearch='propsToSearch'
+      ></BwTable>
+    </bw-card>
+</template>
+
+<script>
+import BwTable from '../../../components/BwTable/BwTable'
+import BwTimePicker from '../../../components/BwTimePicker/BwTimePicker'
+import BwCard from '../../../components/BwCard/BwCard'
+import Api from '../../../service/CallHttp'
+    export default {
+        name:'LOVScreen',
+        components:{
+        BwTable,
+        BwTimePicker,
+        BwCard
+        },
+        created () {
+            this.getList();
+        },
+        computed: { 
+            
+        }, 
+        methods: {
+            nextPage() {
+                this.$router.push("LovAdd");
+            },
+            onEditLov(data) {
+                console.log(data);
+                this.$router.push({ name: "LovManage", params: { lovHeaderId: data} });
+            },
+            async getList() {
+                let {data} = await(await Api()).getListLov()
+                this.tableData = data;
+            },
+            
+            // addForm(){
+            //     this.tableData.push({lovHeaderId:'',lovKey:'',descripton:'',createDate:'',createdBy:'',updatedDate:'',updatedBy:'',isDelete:''})
+            // }
+                 
+        },
+       
+        data() {
+            return {
+                from: {
+                    lovKey:''
+                },
+                textCustom:"จัดการข้อมูล",
+                hiddenButtonCustom: true,
+                tableData: [],
+                row:'row',
+                col:'col-md-12',
+                lableButton: "จัดการข้อมูล",
+                propsToSearch:["LovKey", "descripton"],
+                tableColumns: [
+                   
+                    {
+                         prop: 'lovKey',
+                         label: 'LovKey', 
+                         minWidth: 200,
+                        //  type:'input'
+                    },
+                    {
+                         prop: 'descripton',
+                         label: 'descripton', 
+                         minWidth: 200,
+                        //  type:'input'
+                    },
+                    
+                ],
+                onClickAdd: {
+                onClick: this.nextPage,
+                text: "Add"
+                },
+                onClickTopCuttom:{
+                text: "จัดการข้อมูล",
+                onClick: this.onEditLov,
+                },  
+            }
+        },
+        mounted() {
+            
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
