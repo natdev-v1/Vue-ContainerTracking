102b26203ce74ca6f85d5712a1c7ce129932eeeb
sarawutduangdee <sarawutduangdee@gmail.com>
2020-03-13 17:53:16 +0700

    updateM

--- src/screens/Report/VGM_LGScreen/VGMScreen.vue
+++ src/screens/Report/VGM_LGScreen/VGMScreen.vue
@@ -1,15 +1,86 @@
 <template>
-    <div>
-
+  <div class="col-md-12 card">
+    <div class="row mt-3 mb-3">
+        <div class="col">
+            <button
+            @click='goBack'
+            type="button"
+            class="btn btn-default"
+            ><span class="btn-label"><i class="nc-icon nc-minimal-left"></i></span>
+            ย้อนกลับ</button>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <fg-input
+                label="Test"
+                placeholder="Test">
+                </fg-input>
+            </div>
+        </div>
     </div>
+    <div>
+      <BwTable 
+      @onEdit='addMTDR'
+      :textCustom='textCustom'
+      :hiddenButtonCustom='hiddenButtonCustom'
+      :hiddenOder='hiddenOder'
+      :tableData='tableData'
+      :tableColumns='tableColumns'
+      :propsToSearch='propsToSearch'
+      ></BwTable>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import Api from '../../../service/CallHttp'
+import BwTable from '../../../components/BwTable/BwTable'
+import BwCard from '../../../components/BwCard/BwCard'
+export default {
+  name:'vgms',
+  components: {
+      BwTable,
+      BwCard,
+  },
+  async created() {
+    
+  },
+   data() {
+    return {
+      props: {
+        visible: {
+          type: Boolean,
+          default: false
+        }
+      },
+      isVisible: this.visible,
+      tableData: [],
+      propsToSearch:[],
+      tableColumns: [
+                    {
+                         prop: '',
+                         label: 'Test', 
+                         minWidth: 100,
+                    },
+                    {
+                         prop: '',
+                         label: 'Test', 
+                         minWidth: 100,
+                    },
+
+                    
+                ],
+
+    };
+  },
+    methods: {
+        goBack(){
+          this.$router.push("/truckBook/truckBookData")
+        }
+
+      }
+
+
+}
 </script>
 
-<style scoped>
-
-</style>
